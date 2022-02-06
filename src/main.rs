4ce94d4655668a72a545f7911a22fb8bd4dc1320
SazidAhmed <sazidahmed.official@gmail.com>
2022-02-06 22:17:44 +0600

    ownership completed

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,12 @@
-use std::io;
-use std::{i32};
+fn main() {
+    let s1 = String::from("hello");
 
-fn check_prime(num1:i32) -> bool {
-    if num1 == 0 || num1 == 1 {
-        return false;
-    }else{
-        let mut i:i32 = 1;
-        while i < num1{
-            if num1 % i == 0{
-                return true;
-            }else {
-                return false;
-            }
-            i+=1;
-        }
-    }
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{}' is {}.", s2, len);
 }
 
-fn main() {
-    println!("Insert a positive integer : ");
-    let mut num1 = String::new();
-    io::stdin().read_line(&mut num1)
-    .expect("Data should be a number");
-
-    let a: i32 = num1.trim().parse().ok().expect("Data should be a number");
-
-    let is_prime: bool = check_prime(a);
-
-    println!("Prime {}", is_prime);
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() returns the length of a String
+    (s, length)
 }
